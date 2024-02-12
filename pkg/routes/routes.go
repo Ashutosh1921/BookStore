c3561ae1fa9bba24bc39bad0c2568263ad568048
Ashutosh1921 <as7857139@gmail.com>
2024-02-13 00:16:43 +0530

    second

---
+++ pkg/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/Ashutosh1921/BookStore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var registerbookstoreRoutes = func(router mux.Router) {
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookid).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+
+}
